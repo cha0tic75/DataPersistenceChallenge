8c98193a0a00115bf2f0ddac31aabc9129547620
Tim McCune <tim.mccune1975@gmail.com>
2021-11-09 20:50:43 -0800

    Started to screen fader

---
+++ Assets/Scripts/MyCode/SceneHandler.cs
@@ -0,0 +1,46 @@
+// ######################################################################
+// SceneHandler - Script description goes here
+//
+// Written by Tim McCune <tim.mccune1975@gmail.com>
+// ######################################################################
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project
+{
+	public class SceneHandler : MonoBehaviour
+	{
+
+
+		#region Internal State Field(s):
+		
+		#endregion
+		
+		#region Properties:
+		
+		#endregion
+
+		#region MonoBehaviour Callback Method(s):
+		private void Start()
+		{
+			
+		}
+
+		private void Update()
+		{
+			
+		}
+		#endregion
+		
+		#region Public API:
+		
+		#endregion
+
+		#region Internally Used Method(s):
+		
+		#endregion
+	}
+}
